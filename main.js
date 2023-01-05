1bf6a4567e20c8c436103eec98b12c4432388827
good-boy-iboibo <49881797+good-boy-iboibo@users.noreply.github.com>
2023-01-05 20:52:31 +0900

    Update main.js

--- main.js
+++ main.js
@@ -289,6 +289,7 @@ const chords = [
 
 const notes = [
     'C', 'D', 'E', 'F', 'G', 'A', 'B',
+    'C', 'D', 'E', 'F', 'G', 'A', 'B',
     'Db', 'Eb', 'Gb', 'Ab', 'Bb',
     'C#', 'D#',  'F#', 'G#', 'A#',
 ]
